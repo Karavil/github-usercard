10bb636d5720abe0a8da0f8829a25d818dadb6a0
Alex Karavil <alex@karavil.io>
2020-02-13 15:25:00 -0500

    Implemented automatic fetching of user's followers, then creating cards with the information of each follower

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,72 +1,8 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-const GITHUB_API = 'https://api.github.com';
-const GITHUB_USERNAME = 'Karavil';
-
-const cards = document.querySelector('.cards');
-
-axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME)
-   .then(function (response) {
-      // handle success
-      console.log(response);
-      cards.appendChild(createUserCard(response.data));
-      
-   })
-   .catch(function (error) {
-      // handle error
-      console.log(error);
-   });
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
 /**
  * Accepts a user object that holds values pertaining to their GitHub profile
  * @param {object} user 
  */
-function createUserCard(user) {
+function UserCard(user) {
    const card = document.createElement('div');
    card.classList.add('card');
 
@@ -127,10 +63,32 @@ function createUserCard(user) {
    return card;
 }
 
-/* List of LS Instructors Github username's:
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+function appendUserCard(user_login) {
+   axios.get(GITHUB_API + '/users/' + user_login)
+      .then(user => {
+         // After getting info, create a card with it
+         cards.appendChild(UserCard(user.data));
+      })
+      .catch(function (error) {
+         // Handle error in case we had an error fetching user info
+         console.log('Error fetching follower info:', error);
+      });
+}
+
+const GITHUB_API = 'https://api.github.com';
+const GITHUB_USERNAME = 'Karavil';
+
+const cards = document.querySelector('.cards');
+
+// Get followers of the user provided in GITHUB_USERNAME
+axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME + '/followers')
+   .then(followers => {
+      followers.data.forEach((follower) => {
+         // For each follower, get their GitHub Info
+         appendUserCard(follower.login);
+      });
+   })
+   .catch(function (error) {
+      // Handle errors regarding fetching user followers on GitHub
+      console.log('Error fetching follower list:', error);
+   });
