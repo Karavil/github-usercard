197bb1fa53cbd08f18bb884e7491c2289ee6d5b2
Alex Karavil <alex@karavil.io>
2020-02-13 14:29:32 -0500

    Done with fetching user info from GitHub and creating card element with it

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,22 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const GITHUB_API = 'https://api.github.com';
+const GITHUB_USERNAME = 'Karavil';
+
+const cards = document.querySelector('.cards');
+
+axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME)
+   .then(function (response) {
+      // handle success
+      console.log(response);
+      cards.appendChild(createUserCard(response.data));
+      
+   })
+   .catch(function (error) {
+      // handle error
+      console.log(error);
+   });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -35,7 +51,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,7 +62,72 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+/**
+ * Accepts a user object that holds values pertaining to their GitHub profile
+ * @param {object} user 
+ */
+function createUserCard(user) {
+   const card = document.createElement('div');
+   card.classList.add('card');
+
+   //User Image from GitHub
+   const userImage = document.createElement('img');
+   userImage.src = user.avatar_url;
+
+   //Card Info & Components
+   const cardInfo = document.createElement('div');
+   cardInfo.classList.add('card-info');
+
+   //User real name
+   const name = document.createElement('h3')
+   name.classList.add('name');
+   name.textContent = user.name;
+
+   //Username
+   const username = document.createElement('p')
+   username.classList.add('username')
+   username.textContent = user.login;
+
+   //User location
+   const location = document.createElement('p')
+   location.textContent = user.location;
+
+   //Profile link to github
+   const profile = document.createElement('p')
+   const profileLink = document.createElement('a');
+
+   profileLink.href = user.html_url;
+   profileLink.textContent = user.html_url;
+
+   profile.appendChild(profileLink);
+
+   //Follower counts
+   const followers = document.createElement('p')
+   followers.textContent = user.followers;
+   const following = document.createElement('p')
+   following.textContent = user.following;
+
+   //User bio
+   const bio = document.createElement('p')
+   bio.textContent = user.bio;
+
+   //Append all components of cardInfo
+   cardInfo.appendChild(name);
+   cardInfo.appendChild(username);
+   cardInfo.appendChild(location);
+   cardInfo.appendChild(profile);
+   cardInfo.appendChild(followers);
+   cardInfo.appendChild(following);
+   cardInfo.appendChild(bio);
+
+   //Append user image and info to card
+   card.appendChild(userImage);
+   card.appendChild(cardInfo);
+
+   return card;
+}
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
